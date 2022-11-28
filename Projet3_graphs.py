f9c4d81630b3398c87e5b0f0d98d1d7abf6bc2fe
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-28 16:12:51 +0100

    Create Projet3_graphs.py

---
+++ Projet3_graphs.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+import requests
+
+
+st.title('Analyse du réseau de transports en commun de la ville de Rennes')
+st.image('Bus-100x100.png')
+df_bus = pd.read_csv("df_bus_retards.csv")
+
+# Quasiment tous les éléments streamlit peuvent être affichés dans la "sidebar"
+st.sidebar.title("Réseau Star :")
+st.sidebar.write("  2 lignes de métro")
+st.sidebar.write("  Ligne a - lancement Mars 2022")
+st.sidebar.write("  Ligne b – lancement Juillet 2022")
+st.sidebar.write("  Lignes de bus : 152")
+st.sidebar.write("  Stations de vélo en libre service : 57")
+st.sidebar.write("  Parcs relais : 8")
+
+option_ligne = df_bus['ligne'].unique()
+lignes = st.sidebar.multiselect(
+	'Quelle ligne ?',
+	option_ligne, 
+	option_ligne[0]
+	)
+# Table 
+df_bus_ligne = df_bus[df_bus['ligne'].isin(lignes)]
+st.write('trajet ligne')
+df_bus_ligne
+
+option_ligne_2 = df_bus['retard_a'].unique()
+lignes_2 = st.sidebar.multiselect(
+	'retards ?',
+	option_ligne_2, 
+	option_ligne_2[0]
+	)
+# Table 
+df_bus_ligne_2 = df_bus[df_bus['retard_a'].isin(lignes_2)]
+st.write('Retards')
+df_bus_ligne_2
+
+
