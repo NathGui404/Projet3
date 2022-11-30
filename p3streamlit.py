2aaf51c1961c2a88a9ae2c4461c5fc26aff31d3d
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-30 11:06:49 +0100

    Update p3streamlit.py

--- p3streamlit.py
+++ p3streamlit.py
@@ -9,10 +9,17 @@ import requests
 from pathlib import Path
 
 @st.cache
+
+#fonction de chargement des données
 def load_data():
 	bus_data_path = Path() / 'df_bus_retards.csv'
 	data = pd.read_csv(bus_data_path)
 	return data
+#fonction de conversion des secondes en heure min secondes
+def convert(seconds):
+    min, sec = divmod(seconds, 60)
+    hour, min = divmod(min, 60)
+    return "%d:%02d:%02d" % (hour, min, sec)
 
 df_bus=load_data()
 
@@ -33,26 +40,26 @@ with st.sidebar :
 		st.image("reseau rennes.JPG")
 		
 option_ligne = df_bus['ligne'].unique()
-lignes = st.sidebar.multiselect(
-	'Quelle ligne ?',
-	option_ligne, 
-	option_ligne[0]
-)
-#conversion des secondes en heure min secondes
-def convert(seconds):
-    min, sec = divmod(seconds, 60)
-    hour, min = divmod(min, 60)
-    return "%d:%02d:%02d" % (hour, min, sec)
 
+lignes = st.sidebar.selectbox(
+    'Selectionnez une ligne',
+	option_ligne,
+	option_ligne[0])
 
+st.write(option_ligne)
+if option_ligne!='All':
+    	df_bus_ligne = df_bus[df_bus['ligne'].isin(lignes)]
+	df_bus_ligne=df_bus_ligne[df_bus_ligne['retard_a']=='oui'][['ligne','destination','nom_arret','arrivee_theorique','retard_arrivee']].sort_values(by='retard_arrivee',ascending=False)
+	df_bus['retard_arrivee']=df_bus['retard_arrivee'].apply(convert)
+	st.write(df_bus_ligne)
+else:
+    	st.write(df_bus_ligne)
 
 # Table de toutes les lignes de bus et du retard a chaque arret
 
-df_bus_ligne = df_bus[df_bus['ligne'].isin(lignes)]
+
 st.subheader("Retards de bus les plus importants par ligne et arrêt")
-df_bus_ligne=df_bus_ligne[df_bus_ligne['retard_a']=='oui'][['ligne','destination','nom_arret','arrivee_theorique','retard_arrivee']].sort_values(by='retard_arrivee',ascending=False)
-df_bus['retard_arrivee']=df_bus['retard_arrivee'].apply(convert)
-df_bus_ligne
+
 
 option_tard = df_bus['retard_a'].unique()
 tard = st.sidebar.multiselect(
