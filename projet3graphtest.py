00ae2e54fe16e3019457b78e2c72ce4597aafab6
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-30 21:50:22 +0100

    Update projet3graphtest.py

--- projet3graphtest.py
+++ projet3graphtest.py
@@ -27,12 +27,16 @@ lignes = st.sidebar.selectbox(
 	'Choix ligne de bus ?',
 	option_ligne
 	)
+#map
+
+df_bus_tard = df_bus[(df_bus['ligne']==lignes)&(df_bus['retard_a']=='oui')]
+st.write('Retards supérieurs à 3 minutes')
+st.map(df_bus_tard[['latitude','longitude']])
+
 # Table 
 df_bus_ligne = df_bus[df_bus['ligne']==lignes]
 st.write('Retards les plus importants par ligne de bus et par arrêt')
 df_bus_ligne=df_bus_ligne[df_bus_ligne['retard_a']=='oui'][['ligne','destination','nom_arret','arrivee_theorique','retard_arrivee']].sort_values(by='retard_arrivee',ascending=False)
 df_bus_ligne
 
-df_bus_tard = df_bus[(df_bus['ligne']==lignes)&(df_bus['retard_a']=='oui')]
-st.write('Retards supérieurs à 5 minutes')
-st.map(df_bus_tard[['latitude','longitude']])
+
