f190decd66a6a92668ed3cac479bd6a2a4d67063
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-11-30 16:44:10 +0100

    Update projet3_graphs.py

--- projet3_graphs.py
+++ projet3_graphs.py
@@ -33,7 +33,8 @@ st.write('Retards les plus importants par ligne de bus et par arrêt')
 df_bus_ligne=df_bus_ligne[df_bus_ligne['retard_a']=='oui'][['ligne','destination','nom_arret','arrivee_theorique','retard_arrivee']].sort_values(by='retard_arrivee',ascending=False)
 df_bus_ligne
 
+df_bus_tard = df_bus[(df_bus['ligne']==lignes)&(df_bus['retard_a']=='oui')]
 st.write('Retards supérieurs à 5 minutes')
-st.map(df_bus_ligne[['latitude','longitude']])
+st.map(df_bus_tard[['latitude','longitude']])
 
 
